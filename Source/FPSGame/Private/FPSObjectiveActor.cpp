6e1b9ec8021e589a0afa34e1fd8382865f73a1c2
Karl Eklund <localpart@gmail.com>
2020-10-07 21:25:56 +0200

    10 done

--- Source/FPSGame/Private/FPSObjectiveActor.cpp
+++ Source/FPSGame/Private/FPSObjectiveActor.cpp
@@ -2,6 +2,7 @@
 
 
 #include "FPSObjectiveActor.h"
+#include "Components/SphereComponent.h"
 
 // Sets default values
 AFPSObjectiveActor::AFPSObjectiveActor()
@@ -9,6 +10,11 @@ AFPSObjectiveActor::AFPSObjectiveActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	RootComponent = MeshComp;
+	
+	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	SphereComp->SetupAttachment(MeshComp);
 }
 
 // Called when the game starts or when spawned
@@ -22,6 +28,7 @@ void AFPSObjectiveActor::BeginPlay()
 void AFPSObjectiveActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	std::cout << "tick";
 
 }
 
